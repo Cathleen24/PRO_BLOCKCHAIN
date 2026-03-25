8728a819bae92161eed145c44d22cbff5298c88a
Cathleen24 <cathleenbartolome5@gmail.com>
2026-03-25 13:39:03 +0800

    updated

--- BarangayProgramM11.sol
+++ BarangayProgramM11.sol
@@ -4,16 +4,33 @@ pragma solidity ^0.8.0;
 
 contract BarangayProgram {
     string public programName;
-    uint256 public startingBalance;
+    uint256 public currentBalance;
+    address public captain; // The person who deployed the contract
 
-    // The constructor must be inside the curly braces
+    // Event to log spending for the community to see
+    event FundsSpent(uint256 amount, uint256 remainingBalance);
+
     constructor(string memory _programName, uint256 _startingBalance) {
         programName = _programName;
-        startingBalance = _startingBalance;
+        currentBalance = _startingBalance;
+        captain = msg.sender; // Sets the deployer as the "Captain"
     }
 
-    // Function to retrieve the program info
+    // Modifier to ensure only the Captain can run certain functions
+    modifier onlyCaptain() {
+        require(msg.sender == captain, "Only the Barangay Captain can authorize this.");
+        _;
+    }
+
     function getProgramDetails() public view returns (string memory, uint256) {
-        return (programName, startingBalance);
+        return (programName, currentBalance);
+    }
+
+    // Now protected by 'onlyCaptain'
+    function spendFunds(uint256 _amount) public onlyCaptain {
+        require(_amount <= currentBalance, "Insufficient funds.");
+        currentBalance -= _amount;
+        
+        emit FundsSpent(_amount, currentBalance); // Records the transaction
     }
 }
