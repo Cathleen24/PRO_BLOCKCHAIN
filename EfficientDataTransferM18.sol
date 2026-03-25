2e0701cb90e0c87963dbad6a696ab2d9c5266d0c
Cathleen24 <cathleenbartolome5@gmail.com>
2026-03-25 14:38:58 +0800

    m18

---
+++ EfficientDataTransferM18.sol
@@ -0,0 +1,14 @@
+// https://github.com/Cathleen24/PRO_BLOCKCHAIN
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract EfficientDataTransfer {
+    
+    function echoData(string calldata data) external pure returns (string memory) {
+        return data;
+    }
+
+    function memoryData(string memory data) public pure returns (string memory) {
+        return data;
+    }
+}
