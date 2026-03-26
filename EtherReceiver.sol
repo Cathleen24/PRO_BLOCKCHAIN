db5e3411904e6ce605b8dfa0f33adb429937d1d6
Cathleen24 <cathleenbartolome5@gmail.com>
2026-03-26 17:17:51 +0800

    m20

---
+++ EtherReceiver.sol
@@ -0,0 +1,16 @@
+// https://github.com/Cathleen24/PRO_BLOCKCHAIN
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract EtherReceiver {
+    event PaymentReceived(address indexed from, uint256 amount);
+
+    function receivePayment() public payable {
+        emit PaymentReceived(msg.sender, msg.value);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
